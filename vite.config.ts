3c9f5ae15d0a893ca1fd0caab82e0fe253f0c8d1
David <davidjamesdelacruz.basc@gmail.com>
2025-05-10 18:32:12 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {

8b569fef4ea41e75318fd9ad0d0d7faacbe3fcf7
David <davidjamesdelacruz.basc@gmail.com>
2025-05-10 19:14:49 +0800

    udpaate ule

--- src/pages/Places.tsx
+++ src/pages/Places.tsx
@@ -2,7 +2,7 @@ import { KmlFileList } from "@/components/KmlFileList";
 
 export default function Map() {
   return (
-    <div className="bg-gradient-to-tl from-purple-50 to-fuchsia-100 min-h-screen">
+    <div className="bg-gradient-to-tl from-purple-50 to-fuchsia-100 min-h-screen p-10">
       <div className="container mx-auto py-8 font-poppins ">
         <h1 className="text-3xl font-bold mb-6 text-purple-950">
           Community Resource Maps

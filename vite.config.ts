abbf2fa2b0fefd5ad8f4d2b3f6318b837796047b
David <davidjamesdelacruz.basc@gmail.com>
2025-05-10 15:28:09 +0800

    Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ever-well-care/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
